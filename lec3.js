9463bae2b63adb9a93305193d8277658626ad1a6
Arkopravo Ghosh <arkopravoghosh789.com>
2024-12-27 20:32:55 +0530

    functions

--- lec3.js
+++ lec3.js
@@ -2,53 +2,54 @@
 
 
 // Function Declaration
-// function multiply(a, b) {
-//     return a * b;
-// }
-// console.log(multiply(2, 5));
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(2, 5));
 
 
 // Function Expression - storing function in a variable
-// const multipleOfTwoNumber = function multiply(a, b) {
-//     return a * b;
-// }
-// console.log(multipleOfTwoNumber(2, 5));
+const multipleOfTwoNumber = function multiply(a, b) {
+    return a * b;
+}
+console.log(multipleOfTwoNumber(2, 5));
 
 
 // Anonymous Function - Function Expression storing function in a variable with no function name
-// const multiply = function (a, b) {
-//     return a * b;
-// }
-// console.log(multiply(2, 5));
+const multiply = function (a, b) {
+    return a * b;
+}
+console.log(multiply(2, 5));
 
 
 
 // First Class Functions - Functions treated as a variable and can be passed as an argument to another function
-// function sqaure (num) {
-//     return num * num;
-// }
+function sqaure (num) {
+    return num * num;
+}
 
-// function displaySqaure(fn) {
-//     console.log("Square is " + fn(5));
-// }
+function displaySqaure(fn) {
+    console.log("Square is " + fn(5));
+}
 
-// displaySqaure(sqaure);
+displaySqaure(sqaure);
 
 
 
 
 // IIFE - Immediately Invoked Function Expression -> Function that is executed immediately after it is created
-// (function square(num) {
-//     console.log(num*num);
-// })(6);
+(function square(num) {
+    console.log(num*num);
+})(6);
+
 
 // Example : IIFE
 // ############### Inner functions can access the outer function's variables ###############
-// (function (x) {
-//     return (function (y) {
-//         console.log(x)  // output: 1
-//     })(2);
-// })(1);
+(function (x) {
+    return (function (y) {
+        console.log(x)  // output: 1
+    })(2);
+})(1);
 
 
 
@@ -57,75 +58,75 @@
 
 
 // Example 
-// for(let i=0; i<=5; i++) {
-//     setTimeout(function() {
-//         console.log(i);
-//     }, i*1000);
-// }
+for(let i=0; i<=5; i++) {
+    setTimeout(function() {
+        console.log(i);
+    }, i*1000);
+}
 
 
 
 // Hoisting - in case of function, function is hoisted completed at the top
-// hello();
-// function hello() {
-//     console.log("hello")
-// };
+hello();
+function hello() {
+    console.log("hello")
+};
 
 
 // Example
-// var x = 20;
-// var fnc = function() {
-//     console.log(x)  // output = undefined
-//     var x = 30; 
-// }
+var x = 20;
+var fnc = function() {
+    console.log(x)  // output = undefined
+    var x = 30; 
+}
 
-// fnc();
+fnc();
 
 
 
 
 // Params and arguments: 
-// function add (parameters){
-//     console.log(parameters);
-// }
+function add (parameters){
+    console.log(parameters);
+}
 
-// add("arguments");
+add("arguments");
 
 
 
 
 // Spread operator vs rest operator
-// function add(...nums) { // rest operator
-//     console.log(nums[0] + nums[1]);
-// }
+function add(...nums) { // rest operator
+    console.log(nums[0] + nums[1]);
+}
 
-// var arr = [5, 6];
-// add(...arr);  // spread operator
+var arr = [5, 6];
+add(...arr);  // spread operator
 
 
 // Example
-// const fn = (a, x, y, ...nums) => {
-//     console.log(a, y, nums)
-// }
-// let a = [1,2,3,4,5,6,7]
-// fn(...a)
+const fn = (a, x, y, ...nums) => {
+    console.log(a, y, nums)
+}
+let a = [1,2,3,4,5,6,7]
+fn(...a)
 
 
 
 // callback function -> function that is passed as an argument to another function
-// document.addEventListener("click", function(params) {
-//     console.log("clicked");
-// })
+document.addEventListener("click", function(params) {
+    console.log("clicked");
+})
 
 
 
 
 // Arrow function
-// const add = (a, b) => {
-//     return a + b;
-// }
-// let ans = add(2, 3);
-// console.log(ans);
+const add = (a, b) => {
+    return a + b;
+}
+let ans = add(2, 3);
+console.log(ans);
 
 
 
