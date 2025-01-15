3f23260b6bad1a68b938856a6a14b189261c3923
Arkopravo Ghosh <arkopravoghosh789.com>
2025-01-15 11:06:31 +0530

    promise done

--- lec8.js
+++ lec8.js
@@ -36,3 +36,52 @@
 
 
 // ################ Nested callbacks === Callback hell  ##############################
+
+// Promises Chaining
+
+// const firstPromise = new Promise((resolve, reject) => {
+//     resolve('First!');
+// });
+
+// const secondPromise = new Promise((resolve, reject) => {
+//     resolve(firstPromise);
+// })
+
+// secondPromise
+//     .then(res => res)
+//     .then(res => console.log(res));
+
+
+
+
+
+    
+// function loadJson(url) {
+//     return fetch(url)
+//         .then(response => {
+//             if (response.status == 200) {
+//               return response.json();
+//             } else {
+//               throw new Error(response.status);
+//             }
+//         }
+//     );
+// }
+
+// loadJson('https://javascript.info/no-such-user.json').catch(alert); 
+
+
+
+
+async function loadJson(url) { // (1)
+    let response = await fetch(url); // (2)
+  
+    if (response.status == 200) {
+      let json = await response.json(); // (3)
+      return json;
+    }
+  
+    throw new Error(response.status);
+}
+  
+loadJson('https://javascript.info/no-such-user.json').catch(alert); 
